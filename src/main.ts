d97583efb9b3780226df53d400ba5821189da6bd
christopher ramirez <angel.rmarin@outlook.com>
2023-01-30 15:50:08 -0600

    test again

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
 import { ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-  const PORT = process.env.PORT;
   app.setGlobalPrefix('api/v2');
   app.useGlobalPipes(
     new ValidationPipe({
@@ -19,6 +16,6 @@ async function bootstrap() {
       },
     }),
   );
-  await app.listen(PORT);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
